553f3ba1f1f2c56aea2592b2d85c484302bb00b2
omarabdelaziz10 <120586183+omarabdelaziz10@users.noreply.github.com>
2023-02-06 13:58:20 +0200

    change the env

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "udagram-api-dev.eba-gmumqs33.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev.eba-gmumqs33.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
